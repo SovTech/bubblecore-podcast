6f9c5c928adc48e716fd633bc2c5eaaa62647c7a
Nusrath Khan <nusrath501khan@gmail.com>
2016-11-04 09:38:54 +0200

    add plugin.yaml

--- Plugin.php
+++ Plugin.php
@@ -20,73 +20,58 @@ class Plugin extends PluginBase
             'name'        => 'Podcast',
             'description' => 'Create and publish a podcast using the October CMS platform.',
             'author'      => 'Bubblecore',
-	    	'homepage'	  => 'http://bubblecore.net',
+	    	    'homepage'	  => 'http://bubblecore.net',
             'icon'        => 'icon-play-circle'
         ];
     }
 
     public function registerComponents() {
-		return [
-		    'Bubblecore\Podcast\Components\Podcast' => 'podcast',
-		    'Bubblecore\Podcast\Components\PodcastList' => 'podcastList',
-		    'Bubblecore\Podcast\Components\PodcastEpisode' => 'podcastEpisode',
-		    'Bubblecore\Podcast\Components\Servlet' => 'servlet'
-		];
+    		return [
+    		    'Bubblecore\Podcast\Components\Podcast' => 'podcast',
+    		    'Bubblecore\Podcast\Components\PodcastList' => 'podcastList',
+    		    'Bubblecore\Podcast\Components\PodcastEpisode' => 'podcastEpisode',
+    		    'Bubblecore\Podcast\Components\Servlet' => 'servlet'
+    		];
     }
 
     public function registerNavigation() {
-		return [
-		    'podcast' => [
-				'label' 	=> 'Podcasts',
-				'url' 		=> Backend::url('bubblecore/podcast/channels'),
-				'icon'		=> 'icon-play-circle',
-				'permissions'	=> ['bubblecore.podcast.*'],
-				'order'		=> 500,
-				'sideMenu' => [
-				    'channels'	=> [
-						'label'		=> 'Channels',
-						'icon'		=> 'icon-copy',
-						'url'		=> Backend::url('bubblecore/podcast/channels'),
-						'permissions' 	=> ['bubblecore.podcast.access_channels'],
-				    ],
-				    'episodes' 		=> [
-						'label' 	=> 'Episodes',
-						'icon'		=> 'icon-envelope-o',
-						'url' 		=> Backend::url('bubblecore/podcast/episodes'),
-						'permissions'	=> ['bubblecore.podcast.access_episodes'],
-				    ],
-				]
-		    ]
-		];
-     }
+    		return [
+    		    'podcast' => [
+        				'label' 	=> 'Podcasts',
+        				'url' 		=> Backend::url('bubblecore/podcast/channels'),
+        				'icon'		=> 'icon-play-circle',
+        				'permissions'	=> ['bubblecore.podcast.*'],
+        				'order'		=> 500,
+        				'sideMenu' => [
+        				    'channels'	=> [
+            						'label'		=> 'Channels',
+            						'icon'		=> 'icon-copy',
+            						'url'		=> Backend::url('bubblecore/podcast/channels'),
+            						'permissions' 	=> ['bubblecore.podcast.access_channels'],
+        				    ],
+        				    'episodes' 		=> [
+            						'label' 	=> 'Episodes',
+            						'icon'		=> 'icon-envelope-o',
+            						'url' 		=> Backend::url('bubblecore/podcast/episodes'),
+            						'permissions'	=> ['bubblecore.podcast.access_episodes'],
+        				    ],
+        				]
+    		    ]
+    		];
+    }
 
-	public function registerSettings()
-	{
-	    return [
-	        'settings' => [
-	            'label'       => 'Podcast Settings',
-	            'description' => 'Manage podcast based settings.',
-	            'category'    => 'Misc',
-	            'icon'        => 'icon-play-circle',
-	            'class'       => 'Bubblecore\Podcast\Models\Settings',
-	            'order'       => 500,
-	            'keywords'    => 'security podcast'
-	        ]
-	    ];
-	}
-
-    // /**
-	// * Register new Twig variables
-	// * @return array
-	// */
-	// public function registerMarkupTags()
-	// {
-	// 	return [
-	// 		'functions' => [
-	// 			'form_select_category' => ['Bubblecore\Podcast\Models\Category', 'formSelect'],
-	// 			'form_select_subcategory' => ['Bubblecore\Podcast\Models\Subcategory', 'formSelect'],
-	// 		]
-	// 	];
-	// }
+  	public function registerSettings() {
+  	    return [
+  	        'settings' => [
+  	            'label'       => 'Podcast Settings',
+  	            'description' => 'Manage podcast based settings.',
+  	            'category'    => 'Misc',
+  	            'icon'        => 'icon-play-circle',
+  	            'class'       => 'Bubblecore\Podcast\Models\Settings',
+  	            'order'       => 500,
+  	            'keywords'    => 'security podcast'
+  	        ]
+  	    ];
+  	}
 
 }
